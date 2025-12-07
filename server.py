44ea44064af13a933eb011e2cd2e02dafa0d9d2d
arlauwer <lauwersarno2@outlook.com>
2025-12-07 23:02:49 +0100

    events stock html cleaning up

--- server.py
+++ server.py
@@ -4,8 +4,6 @@ import asyncio
 import random
 import threading
 import time
-from collections import deque
-from dataclasses import dataclass, asdict, field
 
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
@@ -18,12 +16,14 @@ HISTORY_FILE = "static/stock.csv"
 TRADE_LOG_FILE = "static/trade_log.txt"
 
 INTERVAL = 1  # interval in seconds, must be a divisor or multiple of 60 (1, 2, 3, 4, 5, 10, 15, 20, 30, 60, 120, ...)
-INITIAL_PRICE = 10
-MIN_PRICE = 1
-MAX_PRICE = 100
-BASE_VOLATILITY = 0.03  # intrinsic randomness in the price
+BASE_PRICE = 10
+MIN_PRICE = 2  # soft limit, will slowly return to INITIAL_PRICE if exceeded
+MAX_PRICE = 50  # soft limit, will slowly return to INITIAL_PRICE if exceeded
+MU_RETURN = 0.05  # soft return average
+BASE_SIGMA = 0.03  # intrinsic randomness in the price
 EXCHANGE_IMPACT = 0.01  # how much buying/selling affects the price
 APPEND_ON_FREEZE = False  # continue the market but at the same price
+LOAD_FROM_FILE = True  # load the history from the csv
 
 
 # ----------------------------- Market Engine ------------------------------
@@ -32,15 +32,30 @@ state_lock = threading.RLock()
 
 
 class MarketEngine:
+
     def __init__(self):
         self.times = list()
         self.prices = list()
-        self.frozen = False
+        self.event = Event.make_empty()  # only one event at a time
 
-        self.append(time.time(), INITIAL_PRICE)
+        self.frozen = False
+        self.allow_return = True  # Arnoh
+        self.returning = 0  # -1, 0, 1 (down, off, up)
+
+        if LOAD_FROM_FILE:
+            self.load_from_file()
+        if len(self.times) == 0:
+            self.append(time.time(), BASE_PRICE)
 
         if not os.path.exists(TRADE_LOG_FILE):
             open(TRADE_LOG_FILE, "w").close()
+
+    def load_from_file(self):
+        with open(HISTORY_FILE, "r", encoding="utf-8") as f:
+            for line in f.readlines():
+                parts = line.split()
+                self.times.append(float(parts[0]))
+                self.prices.append(float(parts[1]))
 
     def price(self):
         return self.prices[-1]
@@ -61,46 +76,118 @@ class MarketEngine:
             price = self.price()
 
             if not self.frozen:
-                price = self.updatePrice(price)
+                price = self.updatePrice(time, price)
 
             if not self.frozen or APPEND_ON_FREEZE:
                 self.append(time, price)
 
-    def updatePrice(self, price):
-        noise_factor = random.gauss(0, BASE_VOLATILITY)
+    def updatePrice(self, time, price):
+        mu = 0
+        if self.allow_return:
+            if price > MAX_PRICE:
+                self.returning = -1
+            if price < MIN_PRICE:
+                self.returning = 1
+            if self.returning > 0 and price > BASE_PRICE:
+                self.returning = 0
+            if self.returning < 0 and price < BASE_PRICE:
+                self.returning = 0
+            mu += self.returning * MU_RETURN
+        noise_factor = random.gauss(mu, BASE_SIGMA)
         price *= (1 + noise_factor)
-        price = self.bound_price(price)
+
+        price = self.event.updatePrice(price)
+
+        price = max(min(price, MAX_PRICE*2), MIN_PRICE/2)  # hard limit price
 
         return price
 
-    def buy(self, pdm: float):
+    def exchange(self, pdm, displayPrice):
         with state_lock:
             price = self.price()
-            impact = EXCHANGE_IMPACT * pdm / (1 + price)
+            impact = pdm * EXCHANGE_IMPACT / (1 + price)
             price *= 1 + impact
-            price = self.bound_price(price)
 
             self.prices[-1] = price
 
-    def sell(self, pdm: float):
+            with open(TRADE_LOG_FILE, "a", encoding="utf-8") as f:
+                f.write(f"{self.time()},{pdm},{displayPrice}\n")
+
+    def queue_event(self, event):
         with state_lock:
-            price = self.price()
-            impact = EXCHANGE_IMPACT * pdm / (1 + price)
-            price *= 1 - impact
-            price = self.bound_price(price)
-
-            self.prices[-1] = price
-
-    def bound_price(self, price):
-        return max(MIN_PRICE, min(MAX_PRICE, price))
+            if self.event.finished or self.event.priority < event.priority:
+                self.event = event
+                if event.finished:
+                    print("event", event.name, "already finished")
+                else:
+                    print("event", event.name, "started")
+            else:
+                print("event", event.name, "ignored")
 
     def to_dict(self):
         return {
             "times": self.times,
             "prices": self.prices,
             "frozen": self.frozen,
+            "allow_return": self.allow_return,
+            "returning": self.returning,
+            "event": self.event.to_dict(),
         }
 
+
+# ----------------------------- Events -----------------------------
+
+class Event:
+    def __init__(self, name, start_tick, dticks, max_mu, priority=1):
+        self.name = name
+        self.tick = -start_tick  # tick since start
+        self.dticks = dticks  # duration of event in ticks
+        self.max_mu = max_mu
+        self.priority = priority
+
+        self.finished = False
+
+    def updatePrice(self, price):
+        if self.finished:
+            return price
+
+        if self.tick >= 0:
+            if self.tick >= self.dticks - 1:
+                self.finished = True
+            mu = self.mu(self.tick / (self.dticks - 1))
+
+            noise_factor = random.gauss(mu, BASE_SIGMA)
+            price *= (1 + noise_factor)
+
+        self.tick += 1
+        return price
+
+    def mu(self, t):
+        # polynomial bump: [0, 1] |-> [0, max_mu]
+        x = (t-0.5)**2
+        return (1 - x / (0.5**3) + x**2 / 0.5**4) * self.max_mu
+
+    def make_empty():
+        event = Event("empty", 0, 0, 0, 0)
+        event.started = True
+        event.finished = True
+        return event
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "tick": self.tick,
+            "dticks": self.dticks,
+            "max_mu": self.max_mu,
+            "priority": self.priority,
+            "finished": self.finished,
+        }
+
+
+EVENTS = {
+    "coffee": Event("coffee", start_tick=3, dticks=5, max_mu=-0.1),
+    "grant": Event("grant", start_tick=3, dticks=5, max_mu=0.1),
+}
 
 # ----------------------------- WebSocket Manager -------------------------
 
@@ -114,7 +201,7 @@ class ConnectionManager:
         self.active.add(websocket)
 
     def disconnect(self, websocket: WebSocket):
-        self.active.discard(websocket)
+        self.active.discard(websocket)  # Mmmmmmmmmmmmmmmilleke
 
     async def broadcast(self, message: dict):
         to_remove = []
@@ -159,21 +246,24 @@ async def websocket_endpoint(websocket: WebSocket):
 
             mtype = msg.get("type")
 
+            if mtype == "exchange":
+                pdm = float(msg.get("pdm", 0))
+                displayPrice = float(msg.get("displayPrice", 0))
+                if not engine.frozen:
+                    engine.exchange(pdm, displayPrice)
+                    await manager.broadcast({"type": "engine", "engine": engine.to_dict()})
+
             if mtype == "freeze":
-                engine.frozen = bool(msg.get("freeze", False))
+                engine.frozen = not engine.frozen
                 await manager.broadcast({"type": "engine", "engine": engine.to_dict()})
 
-            if mtype == "buy":
-                pdm = float(msg.get("pdm", 0))
-                if not engine.frozen:
-                    engine.buy(pdm)
-                    await manager.broadcast({"type": "engine", "engine": engine.to_dict()})
+            if mtype == "allow-return":
+                engine.allow_return = not engine.allow_return
+                await manager.broadcast({"type": "engine", "engine": engine.to_dict()})
 
-            if mtype == "sell":
-                pdm = float(msg.get("pdm", 0))
-                if not engine.frozen:
-                    engine.sell(pdm)
-                    await manager.broadcast({"type": "engine", "engine": engine.to_dict()})
+            if mtype == "event":
+                event = EVENTS[msg.get("event")]
+                engine.queue_event(event)
 
     except Exception:
         pass
@@ -186,7 +276,7 @@ async def websocket_endpoint(websocket: WebSocket):
 
 async def broadcaster_loop():
     if INTERVAL < 60:
-        if 60 % INTERVAL != 0:
+        if 60 % INTERVAL != 0:  # Boh
             raise ValueError("Sub-minute interval must divide 60.")
     else:
         if INTERVAL % 60 != 0:
@@ -202,14 +292,13 @@ async def broadcaster_loop():
             # Sub-minute
             slot = lt.tm_sec // INTERVAL
             key = (lt.tm_hour, lt.tm_min, slot)
-
         else:
             # Full-minute
             minutes_per_slot = INTERVAL // 60
             slot = lt.tm_min // minutes_per_slot
             key = (lt.tm_hour, slot)
 
-        if key != last_slot:
+        if key != last_slot:  # Leneh
             last_slot = key
             engine.tick(now)
             await manager.broadcast({"type": "engine", "engine": engine.to_dict()})
